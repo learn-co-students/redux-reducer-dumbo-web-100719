e0727d7e1e4488dac60ad25b2bfd3d2b9f75aad5
Hamidou Diallo <Dio.hamidou@gmail.com>
2020-01-04 16:10:38 -0500

    Done

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,21 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case "ADD_FRIEND":
+      return { friends: [...state.friends, action.friend] };
+
+    case "REMOVE_FRIEND":
+      const indexRemoval = state.friends.findIndex(
+        friend => friend.id === action.id
+      );
+      return {
+        ...state,
+        friends: [
+          ...state.friends.slice(0, indexRemoval),
+          ...state.friends.slice(indexRemoval + 1)
+        ]
+      };
+
+    default:
+      return state;
+  }
 }
