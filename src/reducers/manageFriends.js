cd0fd648e998d188e90b44d2ddcc884945ea0078
Hope <hopegiometti@gmail.com>
2020-01-06 10:19:59 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,20 @@
 export function manageFriends(state, action){
+   
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            return {...state, friends:[...state.friends, {name: "Joe", hometown: "Boston", id: 101}]}
+            
+        case 'REMOVE_FRIEND':
+            const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+            return (
+              {...state,
+                  friends: [
+                    ...state.friends.slice(0, removalIndex),
+                    ...state.friends.slice(removalIndex + 1)
+                  ]
+              }
+            )
+        default:
+            return state
+    }
 }
