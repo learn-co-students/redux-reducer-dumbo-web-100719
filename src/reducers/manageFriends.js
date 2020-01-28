8e450743be36145cb16b2782192518fdc7e2e917
Carlie Anglemire <carlieanglemire@Carlies-MacBook-Air.local>
2020-01-28 09:39:47 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,38 @@
-export function manageFriends(state, action){
-}
+import { render } from "react-dom";
+
+let initialState={
+    friends: []
+};
+
+function addFriendAction(the_friend){
+    type: "ADD FRIEND",
+   the_friend
+};
+
+function removeFriendAction(id){
+    type: "REMOVE_FRIEND",
+    id
+};
+
+export function manageFriends(state=initialState, action){
+
+    switch(action.type){
+
+        case 'ADD_FRIEND':
+            return{
+                friends: [...state.friends, action.friend]
+            }
+        
+        case 'REMOVE_FRIEND':
+
+        const friendsFiltered= state.friends.filter(friend=> {
+            return friend.id !== action.id
+        })
+                return{
+                    friends: friendsFiltered
+                }
+    
+        default: return state;
+    }
+};
+
